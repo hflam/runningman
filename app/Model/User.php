982e77ff75afbe76483fdeb412433e627137334c
Alvin <lam_hoi_fai@hotmail.com>
2013-01-12 23:22:05 -0800

    User setup

--- app/Model/User.php
+++ app/Model/User.php
@@ -8,6 +8,18 @@ App::uses('AppModel', 'Model');
  */
 class User extends AppModel {
 
+    /**
+     * Behaviors
+     *
+     * @var array
+     */
+    public $actsAs = array(
+	        'Acl' => array(
+	            'type' => 'requester'
+	        ),
+	        'Containable',
+        );
+
 	/**
 	 * Display field
 	 *
@@ -36,4 +48,57 @@ class User extends AppModel {
 			'order' => ''
 		)
 	);
+	
+	/**
+     * beforeSave method
+     *
+     * It is called before the Model saves the data,
+     * it hashes the password for security reasons.
+     *
+     * @return void
+     */
+    public function beforeSave($options = array()) {
+        if (isset($this->data['User']['password'])) {
+            $this->data['User']['password'] = AuthComponent::password($this->data['User']['password']);
+        }
+        if (isset($this->data['User']['username'])) {
+            $this->data['User']['hash'] = Security::hash($this->data['User']['username'],'sha1',true);
+        }
+        return true;
+    }
+	
+	/**
+     * Creates virtual field
+     *
+     * @param bool $id
+     * @param null $table
+     * @param null $ds
+     */
+    public function __construct($id = false, $table = null, $ds = null) {
+        parent::__construct($id,$table,$ds);
+        $this->virtualFields = array('full_name' => 'CONCAT('.$this->alias.'.`name`, " ", '.$this->alias.'.lastname)');
+    }
+	
+	/**
+     * parentNode method
+     *
+     * Used by the ACL Plugin
+     *
+     * @return array|null
+     */
+    function parentNode() {
+        if (!$this->id && empty($this->data)) {
+            return null;
+        }
+        if (isset($this->data['User']['role_id'])) {
+            $roleId = $this->data['User']['role_id'];
+        } else {
+            $roleId = $this->field('role_id');
+        }
+        if (!$roleId) {
+            return null;
+        } else {
+            return array('Role' => array('id' => $roleId));
+        }
+    }
 }
